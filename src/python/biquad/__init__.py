2c472c2a1a6dbfe424561f8c62bbc662800a9bed
Jürgen Hock <juergen.hock@jurihock.de>
2023-03-13 20:13:00 +0100

    Add LPF and HPF

--- src/python/biquad/__init__.py
+++ src/python/biquad/__init__.py
@@ -3,4 +3,6 @@ __version__ = "0.1"
 from .allpass  import allpass
 from .bandpass import bandpass
 from .biquad   import biquad
+from .highpass import highpass
+from .lowpass  import lowpass
 from .notch    import notch
---
+++ src/python/biquad/highpass.py
@@ -0,0 +1,84 @@
+from .biquad import biquad
+
+import numba
+import numpy
+
+
+class highpass(biquad):
+    """
+    Highpass filter (HPF).
+    """
+
+    def __init__(self, sr):
+
+        self.sr = sr
+
+        self.__call__(0, 1, 2) # warmup numba
+
+    def __call__(self, x, f, q):
+        """
+        Process single or multiple samples at once.
+        """
+
+        scalar = numpy.isscalar(x)
+
+        ba = self.ba
+        xy = self.xy
+
+        x = numpy.atleast_1d(x)
+        y = numpy.zeros(x.shape)
+
+        f = numpy.atleast_1d(f)
+        q = numpy.atleast_1d(q)
+
+        f = numpy.resize(f, x.shape)
+        q = numpy.resize(q, x.shape)
+
+        sr = self.sr
+
+        self.__filter__(ba, xy, x, y, f, q, sr)
+
+        return y[0] if scalar else y
+
+    @staticmethod
+    @numba.jit(nopython=True, fastmath=True)
+    def __filter__(ba, xy, x, y, f, q, sr):
+
+        rs = 2 * numpy.pi / sr
+
+        for i in range(x.size):
+
+            w = f[i] * rs
+
+            cosw = numpy.cos(w)
+            sinw = numpy.sin(w)
+
+            p = sinw / (2 * q[i])
+
+            # update b
+            ba[0, 1] = -(1 + cosw)
+            ba[0, 2] = ba[0, 1] / -2
+            ba[0, 0] = ba[0, 2]
+
+            # update a
+            ba[1, 0] =  1 + p
+            ba[1, 1] = -2 * cosw
+            ba[1, 2] =  1 - p
+
+            # roll x
+            xy[0, 2] = xy[0, 1]
+            xy[0, 1] = xy[0, 0]
+
+            # roll y
+            xy[1, 2] = xy[1, 1]
+            xy[1, 1] = xy[1, 0]
+
+            # update x and y
+            xy[0, 0] = x[i]
+            xy[1, 0] = (ba[0, 0] * xy[0, 0]  + \
+                        ba[0, 1] * xy[0, 1]  + \
+                        ba[0, 2] * xy[0, 2]  - \
+                        ba[1, 1] * xy[1, 1]  - \
+                        ba[1, 2] * xy[1, 2]) / ba[1, 0]
+
+            y[i] = xy[1, 0]
